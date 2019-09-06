b68498e2457a7e0bd219137bdccba4b6858976cf
bonial_virto <svetlana.suslova2031@gmail.com>
2019-09-06 20:01:27 +0300

    added ajax for form 1

--- js/script.js
+++ js/script.js
@@ -40,7 +40,7 @@ window.addEventListener('DOMContentLoaded', function() {
 
 //Timer
 
- let deadline = '2019-07-21';
+ let deadline = '2019-10-21';
 
  function getTimeRemaining(endtime) {
  	let t = Date.parse(endtime) - Date.parse(new Date()),
@@ -100,9 +100,71 @@ window.addEventListener('DOMContentLoaded', function() {
 
      for (let i = 0; i < moreInTab.length; i++) {
      	moreInTab[i].addEventListener('click', function() {
+          this.classList.add('more-splash');
      	overlay.style.display = "block";
      	document.body.style.overflow = "hidden";
      }); 
   }
+
+ // Form
+ let message = new Object();
+ message.loading = "Загрузка...";
+ message.success = "Спасибо! Скоро мы с вами свяжемся";
+ message.failure = "Что-то пошло не так...";
+
+ let form = document.getElementsByClassName('main-form')[0],
+     input = form.getElementsByTagName('input'),
+     statusMessage = document.createElement('div');
+     statusMessage.classList.add('status');
+
+     form.addEventListener('submit', function(event) {
+          event.preventDefault();
+          form.appendChild(statusMessage);
+
+          //AJAX
+          let request = new XMLHttpRequest();
+          request.open("POST", 'server.php');
+          request.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+
+          let formData = new FormData(form);
+          request.send(formData);
+
+          request.onreadystatechange = function() {
+               if (request.readyState < 4)  {
+                    statusMessage.innerHTML = message.loading;
+               } else if (request.readyState === 4) {
+                    if (request.status == 200 && request.status < 300) {
+                         statusMessage.innerHTML = message.success;
+                    } else {
+                         statusMessage.innerHTML = message.failure;
+                    }
+               }
+          }
+
+          for (let i = 0; i < input.length; i++) {
+               input[i].value = '';
+          }
+
+     });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 });
