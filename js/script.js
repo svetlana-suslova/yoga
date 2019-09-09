46e42dce4c0a5c1d2ff4147ccd2c38707eb79eb6
bonial_virto <svetlana.suslova2031@gmail.com>
2019-09-09 21:02:07 +0300

    Added calculator logic

--- js/script.js
+++ js/script.js
@@ -214,13 +214,47 @@ dotsWrap.addEventListener('click', function (event) {
      }
 });
 
+//Calc
+let persons = document.getElementsByClassName('counter-block-input')[0],
+    restDays = document.getElementsByClassName('counter-block-input')[1],
+    place = document.getElementById('select'),
+    totalValue = document.getElementById('total'),
+    personSum = 0,
+    daysSum = 0,
+    total = 0;
 
+    totalValue.innerHTML = 0;
 
+    persons.addEventListener('change', function () {
+         personSum = +this.value;
+         total = (daysSum + personSum) * 2000;
+         if (restDays.value == '') {
+          totalValue.innerHTML = 0;
+         } else {
+          totalValue.innerHTML = total;
+         }
 
+    });
 
+    restDays.addEventListener('change', function () {
+         daysSum = +this.value;
+         total = (daysSum + personSum) * 2000;
+         if (persons.value == '') {
+          totalValue.innerHTML = 0;
+         } else {
+          totalValue.innerHTML = total;
+         }
 
+    });
 
+    place.addEventListener('change', function() {
+     if (restDays.value == '' ||  persons.value == '') {
+          totalValue.innerHTML = 0;
+     } else {
+          let a = total;
+          totalValue.innerHTML = a * this.options[this.selectedIndex].value;
+     }
 
-
+    });
 
 });
